de0aa751bca1e373f8347755ef09ad5de48772e7
nowpassion <billonbill@naver.com>
2025-02-14 14:50:55 +0900

    tale theme에 기능 추가 및 수정
    - toc 추가 및 css 수정 (오른쪽 배치 및 모바일 숨김)
    - 마크다운 리스트 들여쓰기 간격 조정
    - 모바일 페이지에서 본문 너비 고정
    - 테스트 용 서버 실행 스크립트 추가

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@
 
 source "https://rubygems.org"
 gemspec
+
+gem 'jemoji'
+gem 'jekyll-toc'
+
---
+++ run_local_server.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# 프로그램 이름
+PROGRAM_NAME="jekyll"
+
+# 프로그램이 실행 중인지 확인하는 함수
+is_running() {
+    pgrep -f "$PROGRAM_NAME" > /dev/null 2>&1
+    return $?
+}
+
+# 프로그램 실행 함수
+start_program() {
+    if is_running; then
+        echo "프로그램이 이미 실행 중입니다."
+    else
+		$PROGRAM_NAME serve -t --watch -H 192.168.174.10 -b "" -B
+        echo "프로그램을 시작합니다."
+    fi
+}
+
+# 프로그램 종료 함수
+stop_program() {
+    if is_running; then
+        pkill -f "$PROGRAM_NAME"
+        echo "프로그램을 종료합니다."
+    else
+        echo "실행 중인 프로그램이 없습니다."
+    fi
+}
+
+# 명령어 인수 처리
+if [ $# -eq 0 ]; then
+    start_program
+else
+    case $1 in
+        start)
+            start_program
+            ;;
+        stop)
+            stop_program
+            ;;
+        *)
+            echo "사용법: $0 [start|stop]"
+            ;;
+    esac
+fi
+
