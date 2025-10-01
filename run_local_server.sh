dde82f8f4f4a0d96f37538c2ffcdffd66b2bbcd4
nowpassion <billonbill@naver.com>
2025-10-01 17:18:11 +0900

    테스트 서버 실행 스크립트 수정

--- run_local_server.sh
+++ run_local_server.sh
@@ -2,6 +2,12 @@
 
 # 프로그램 이름
 PROGRAM_NAME="jekyll"
+my_ipaddr=""
+
+find_my_ipaddr()
+{
+	my_ipaddr=`ip -o -4 addr show dev enp3s0 | awk '{print $4}' | cut -d/ -f1`
+}
 
 # 프로그램이 실행 중인지 확인하는 함수
 is_running() {
@@ -11,10 +17,12 @@ is_running() {
 
 # 프로그램 실행 함수
 start_program() {
+    daemon_option="$1"
+
     if is_running; then
         echo "프로그램이 이미 실행 중입니다."
     else
-		$PROGRAM_NAME serve -t --watch -H 192.168.174.10 -b "" -B
+		$PROGRAM_NAME serve -w -t -H "$my_ipaddr" -b "" $daemon_option
 		if [ $? -eq 0 ];
 		then
         	echo "프로그램을 시작합니다."
@@ -34,6 +42,8 @@ stop_program() {
     fi
 }
 
+find_my_ipaddr
+
 # 명령어 인수 처리
 if [ $# -eq 0 ]; then
     start_program
@@ -45,8 +55,21 @@ else
         stop)
             stop_program
             ;;
+		restart)
+            stop_program
+			sleep 2
+            start_program
+            ;;
+        start-daemon)
+            start_program "-B"
+            ;;
+        restart-daemon)
+            stop_program
+			sleep 2
+            start_program "-B"
+            ;;
         *)
-            echo "사용법: $0 [start|stop]"
+            echo "사용법: $0 [start|stop|restart|start-daemon|restart-daemon]"
             ;;
     esac
 fi
